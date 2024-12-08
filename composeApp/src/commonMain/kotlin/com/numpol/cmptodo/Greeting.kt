d180e7875507edb0eec2705a798287ab9dc383b8
Numpol Poldongnok <numpol.pol@ascendcorp.com>
2024-12-08 22:56:42 +0700

    Remove unused files

--- composeApp/src/commonMain/kotlin/com/numpol/cmptodo/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.numpol.cmptodo
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/numpol/cmptodo/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.numpol.cmptodo
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
